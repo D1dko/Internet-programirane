f2fafe8db088022449e5aed53902c777403f0166
D1dko <deqnyordanov@mail.bg>
2023-11-24 11:30:43 +0200

    Logout, register and login are working

--- 03.DogsApp-Services/DogsApp/DogsApp/Program.cs
+++ 03.DogsApp-Services/DogsApp/DogsApp/Program.cs
@@ -2,6 +2,7 @@
 using DogsApp.Core.Contracts;
 using DogsApp.Core.Services;
 using DogsApp.Infrastructure.Data;
+using DogsApp.Infrastructure.Data.Domain;
 using DogsApp.Infrastructure.Data.Infrastructure;
 
 using Microsoft.AspNetCore.Identity;
@@ -22,7 +23,7 @@ namespace DogsApp
                 UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
                 options.Password.RequireDigit = false;
